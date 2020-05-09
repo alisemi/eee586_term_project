a30adecf6cceba2e5fd385bd70f7879b589f2779
Ali Semi Yenimol <aliyenimol@gmail.com>
2020-05-09 12:11:17 +0300

    DB connection in memory, network generated

--- main.py
+++ main.py
@@ -3,15 +3,19 @@
 
 import sqlite3
 from sqlite3 import Error
+from tqdm import tqdm
 
-dataset_filename = "../reddit-comments-may-2015/database.sqlite"
+dataset_filename = "../reddit-comments-may-2015/CasualConversations_sub.db"
 recursive_weigh_factor = 1/2
 base_weight = 1
+total_comments = 234694
 
 def recursive_parenting(cur, edges, comment_id, weight):
     # Get the author and parent id of the comment
-    cur.execute("SELECT author,parent_id FROM May2015 WHERE name=?", comment_id)
+    cur.execute("SELECT author,parent_id FROM data WHERE name=?", comment_id)
     parent_comment = cur.fetchall() 
+    if weight*recursive_weigh_factor<0.05: # For efficieny 
+        return
     if parent_comment: # There is a row for the parent comment in DB
         # All comment names(id) are unique, there should be only one result
         parent_comment = parent_comment[0]
@@ -24,10 +28,13 @@ def recursive_parenting(cur, edges, comment_id, weight):
 # cur is cursor object from the database connection
 def generate_graph_data(cur, graph_file_name):
     graph_file = open(graph_file_name, "w")
-    cur.execute("SELECT DISTINCT author FROM May2015")
+    cur.execute("SELECT DISTINCT author FROM data")
     distinct_authors = cur.fetchall() # Fetchall returns a tuple ("author_name",)
-    for author in distinct_authors[1:10]:
-        cur.execute("SELECT name,parent_id FROM May2015 WHERE author=?", author)
+    target = ('[deleted]',)
+    distinct_authors.remove(target)
+    comments_sum = 0
+    for author in tqdm(distinct_authors):
+        cur.execute("SELECT name,parent_id FROM data WHERE author=?", author)
         comments = cur.fetchall() #rows holds ids of all comments made by the author
         edges = {} # Edges for the current author
         for comment in comments:
@@ -39,20 +46,29 @@ def generate_graph_data(cur, graph_file_name):
         edges.pop("[deleted]", None) #delete the edge of author to the [deleted] authors
         for key,val in edges.items():
             print(author[0] + " " + key + " " + str(val), file=graph_file)
+        comments_sum += len(comments)
+        print("\n" + str((comments_sum/total_comments)*100) + "% of total main comments are processed.")
     
     graph_file.close()
+    
 
-if __name__ == '__main__':    
+def connect_db_in_memory(dataset_filename):
     # Dataset is an sqlite Databse
     conn = None
     try:
         conn = sqlite3.connect(dataset_filename)
         print(sqlite3.version)
-        cur = conn.cursor()
-        generate_graph_data(cur, "reddit_network.txt")   
-   
+        # Processed the database file in memory
+        dest = sqlite3.connect(':memory:')
+        conn.backup(dest)
+        return dest
     except Error as e:
         print(e)
-    finally:
-        if conn:
-            conn.close()
+        return None
+
+if __name__ == '__main__':
+    conn = connect_db_in_memory(dataset_filename)
+    cur = conn.cursor()
+    generate_graph_data(cur, "reddit_casualconversation_network.txt") 
+    if conn:
+        conn.close()
